e9606876c0214516585b2720c62cae0474fceaca
joemar-ceneza <joemar.ceneza@gmail.com>
2024-08-05 04:16:12 +0800

    adding slug, image and pre-save hook to automatically generate a slug from the name

--- models/category.js
+++ models/category.js
@@ -9,16 +9,35 @@ const categorySchema = new mongoose.Schema(
       unique: true,
       maxlength: 50,
     },
+    slug: {
+      type: String,
+      unique: true,
+    },
     description: {
       type: String,
       trim: true,
       maxlength: 200,
     },
+    image: {
+      type: String, // url to the product image
+      required: true,
+    },
   },
   {
-    timestamps: true,
+    timestamps: true, // Automatically add createAt and UpdateAt fields
   }
 );
+
+// pre-save hook to automatically generate a slug from the name
+categorySchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = this.name
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  }
+  next();
+});
 
 const Category = mongoose.model("Category", categorySchema);
 
